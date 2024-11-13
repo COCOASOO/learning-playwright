5a0fbe35a13f34b346067514001a6b558ad75a08
Adrian <adrianalarcon720@gmail.com>
2024-11-13 18:10:42 +0100

    parte8: automatizar añadir al carrito de compra 1 producto

--- tests/sauce-demo.spec.ts
+++ tests/sauce-demo.spec.ts
@@ -17,8 +17,27 @@ async function verificarLogin(page: Page): Promise<void> {
   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 }
 
-test('prueba login',async ({page}) => {
+
+async function obtenerProductoRandom(page: Page): Promise<void> {
+  const productos = await page.locator("#inventory_container .inventory_item").all();
+
+  const randomIndex = Math.floor(Math.random() * productos.length);
+
+  const randomItem = productos[randomIndex];
+
+  const expectedName = await randomItem.locator('.inventory_item_name').innerText();
+  const expectedDescription = await randomItem.locator('.inventory_item_desc').innerText();
+  const expectedPrice = await randomItem.locator('.inventory_item_price').innerText();
+  
+  await randomItem.getByRole("button", { name: "Add to cart" }).click();
+
+  await page.locator('a.shopping_cart_link').click()
+}
+
+test("prueba login", async ({ page }) => {
   await abrirPagina(page);
   await ingresarCredenciales(page);
   await hacerLogin(page);
+  await verificarLogin(page);
+  await obtenerProductoRandom(page);
 });
