6c139fca3f756e648f175760012026a81a2f6752
Adrian <adrianalarcon720@gmail.com>
2024-11-19 17:18:38 +0100

    parte11

--- tests/sauce-demo.spec.ts
+++ tests/sauce-demo.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect, Page } from "@playwright/test";
-
+import { LoginPage } from "./pageobjects/loginpage";
 async function abrirPagina(page: Page): Promise<void> {
   await page.goto("https://www.saucedemo.com/");
 }
 
-async function ingresarCredenciales(page: Page): Promise<void> {
-  await page.getByRole("textbox", { name: "Username" }).fill("standard_user");
-  await page.getByRole("textbox", { name: "Password" }).fill("secret_sauce");
-}
-
 async function hacerLogin(page: Page): Promise<void> {
-  await page.getByRole("button", { name: "Login" }).click();
+  const loginPage = new LoginPage(page);
+  await loginPage.fillUsername('problem_user');
+  await loginPage.fillPassword('secret_sauce');
+  await loginPage.clickOnLogin();
 }
 
 async function verificarLogin(page: Page): Promise<void> {
@@ -40,34 +38,36 @@ async function obtenerProductoRandom(page: Page): Promise<void> {
 
   await page.locator("a.shopping_cart_link").click();
 
-  const actualName = await page.locator('.inventory_item_name').innerText();
-  const actualDescription = await page.locator('.inventory_item_desc').innerText();
-  const actualPrice = await page.locator('.inventory_item_price').innerText();
+  const actualName = await page.locator(".inventory_item_name").innerText();
+  const actualDescription = await page
+    .locator(".inventory_item_desc")
+    .innerText();
+  const actualPrice = await page.locator(".inventory_item_price").innerText();
 
   expect(actualName).toEqual(expectedName);
   expect(actualDescription).toEqual(expectedDescription);
   expect(actualPrice).toEqual(expectedPrice);
 
   expect(page.getByRole("button", { name: "Checkout" })).toBeVisible();
-  await page.getByRole('button',{name: 'Checkout'}).click();
+  await page.getByRole("button", { name: "Checkout" }).click();
 
-  await page.getByRole('textbox',{name:'First Name'}).fill('Pedro')
-  await page.getByRole('textbox',{name:'Last Name'}).fill('Almodovar')
-  await page.getByRole('textbox',{name:'Zip/Postal Code'}).fill('89185')
+  await page.getByRole("textbox", { name: "First Name" }).fill("Pedro");
+  await page.getByRole("textbox", { name: "Last Name" }).fill("Almodovar");
+  await page.getByRole("textbox", { name: "Zip/Postal Code" }).fill("89185");
 
-  expect(page.getByRole('button',{name:'Continue'})).toBeVisible();
-  await page.getByRole('button',{name: 'Continue'}).click();
-  await page.getByRole('button',{name: 'Finish'}).click();
+  expect(page.getByRole("button", { name: "Continue" })).toBeVisible();
+  await page.getByRole("button", { name: "Continue" }).click();
+  await page.getByRole("button", { name: "Finish" }).click();
 
-  await expect(page.getByRole('heading',{name: 'Thank you for your order!'})).toBeVisible()
-
+  await expect(
+    page.getByRole("heading", { name: "Thank you for your order!" })
+  ).toBeVisible();
 }
 
 test("prueba login", async ({ page }) => {
   await abrirPagina(page);
-  await ingresarCredenciales(page);
   await hacerLogin(page);
   await verificarLogin(page);
   await obtenerProductoRandom(page);
-  console.log("weelll done")
+  console.log("weelll done");
 });
