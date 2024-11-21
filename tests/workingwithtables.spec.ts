66d4c786c24a87dcc79f1e6ca0b8887fdf9a413f
Adrian <adrianalarcon720@gmail.com>
2024-11-21 17:43:51 +0100

    he añadido un filtro

--- tests/workingwithtables.spec.ts
+++ tests/workingwithtables.spec.ts
@@ -7,25 +7,34 @@ test("test web table", async ({ page }) => {
 
   const rows = await tableContainer.locator("xpath=.//tr").all();
 
-  const countries:Country[]=[]
+  const countries: Country[] = [];
 
   console.log(rows.length);
 
   for (let row of rows) {
-    let country:Country={
-        name: await row?.locator("xpath=.//td[2]").innerText(),
-        capital: await row?.locator("xpath=.//td[3]").innerText(),
-        currency: await row?.locator("xpath=.//td[4]").innerText(),
-        primaryLanguage: await row?.locator("xpath=.//td[5]").innerText()
-    }
+    let country: Country = {
+      name: await row?.locator("xpath=.//td[2]").innerText(),
+      capital: await row?.locator("xpath=.//td[3]").innerText(),
+      currency: await row?.locator("xpath=.//td[4]").innerText(),
+      primaryLanguage: await row?.locator("xpath=.//td[5]").innerText(),
+    };
     countries.push(country);
   }
+  /*
+  for (let country of countries) {
+    console.log(country);
+  }
+  */
 
+  const countrySpanishSpeakers = countries.filter(
+    (country) => country.primaryLanguage === "Spanish"
+  );
+  console.log("PAISES ESPAÑOLES: " , countrySpanishSpeakers);
 });
 
 interface Country {
-    name:string,
-    capital:string,
-    currency:string,
-    primaryLanguage:string
+  name: string;
+  capital: string;
+  currency: string;
+  primaryLanguage: string;
 }
