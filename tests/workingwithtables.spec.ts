8d4f123683d31608715026d90ade10cac3641590
Adrian <adrianalarcon720@gmail.com>
2024-11-21 17:38:26 +0100

    almacenadas toda la tabla con una interfaz

--- tests/workingwithtables.spec.ts
+++ tests/workingwithtables.spec.ts
@@ -7,9 +7,25 @@ test("test web table", async ({ page }) => {
 
   const rows = await tableContainer.locator("xpath=.//tr").all();
 
+  const countries:Country[]=[]
+
   console.log(rows.length);
-  
+
   for (let row of rows) {
-    console.log(await row.innerText());
+    let country:Country={
+        name: await row?.locator("xpath=.//td[2]").innerText(),
+        capital: await row?.locator("xpath=.//td[3]").innerText(),
+        currency: await row?.locator("xpath=.//td[4]").innerText(),
+        primaryLanguage: await row?.locator("xpath=.//td[5]").innerText()
+    }
+    countries.push(country);
   }
+
 });
+
+interface Country {
+    name:string,
+    capital:string,
+    currency:string,
+    primaryLanguage:string
+}
